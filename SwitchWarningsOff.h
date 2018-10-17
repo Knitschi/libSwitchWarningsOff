c43d44a577ab39a56b2fbf9eccce9c37351fa7c4
Knitschi <knitschi@freenet.de>
2018-10-17 22:33:57 +0200

    Add code analysis warnings to the ignore list.

--- SwitchWarningsOff.h
+++ SwitchWarningsOff.h
@@ -18,7 +18,52 @@
                 __pragma(warning(disable:4512)) \
                 __pragma(warning(disable:4800)) \
                 __pragma(warning(disable:4996)) \
-				__pragma(warning(disable:6326))
+                __pragma(warning(disable:6326)) \
+				__pragma(warning(disable:26400)) \
+				__pragma(warning(disable:26401)) \
+				__pragma(warning(disable:26408)) \
+				__pragma(warning(disable:26409)) \
+				__pragma(warning(disable:26426)) \
+				__pragma(warning(disable:26429)) \
+				__pragma(warning(disable:26432)) \
+				__pragma(warning(disable:26434)) \
+				__pragma(warning(disable:26435)) \
+				__pragma(warning(disable:26439)) \
+				__pragma(warning(disable:26440)) \
+				__pragma(warning(disable:26443)) \
+				__pragma(warning(disable:26444)) \
+				__pragma(warning(disable:26446)) \
+				__pragma(warning(disable:26447)) \
+				__pragma(warning(disable:26451)) \
+				__pragma(warning(disable:26455)) \
+				__pragma(warning(disable:26456)) \
+				__pragma(warning(disable:26460)) \
+				__pragma(warning(disable:26461)) \
+				__pragma(warning(disable:26462)) \
+				__pragma(warning(disable:26465)) \
+				__pragma(warning(disable:26466)) \
+				__pragma(warning(disable:26471)) \
+				__pragma(warning(disable:26472)) \
+				__pragma(warning(disable:26473)) \
+				__pragma(warning(disable:26474)) \
+				__pragma(warning(disable:26475)) \
+				__pragma(warning(disable:26476)) \
+				__pragma(warning(disable:26481)) \
+				__pragma(warning(disable:26482)) \
+				__pragma(warning(disable:26485)) \
+				__pragma(warning(disable:26486)) \
+				__pragma(warning(disable:26487)) \
+				__pragma(warning(disable:26489)) \
+				__pragma(warning(disable:26490)) \
+				__pragma(warning(disable:26491)) \
+				__pragma(warning(disable:26492)) \
+				__pragma(warning(disable:26493)) \
+				__pragma(warning(disable:26494)) \
+				__pragma(warning(disable:26495)) \
+				__pragma(warning(disable:26496)) \
+				__pragma(warning(disable:26497)) \
+				__pragma(warning(disable:26498)) \
+
 
 #define SWITCH_WARNINGS_ON __pragma(warning(pop))
 
